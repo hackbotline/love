3a1426060a6fec566131c06a1731b70df6ac5df8
Selfbotman <34406024+Selfbotman@users.noreply.github.com>
2018-01-27 01:39:10 +0700

    Update channel.py

--- LINETCR/Api/channel.py
+++ LINETCR/Api/channel.py
@@ -18,8 +18,8 @@ class Channel:
     http_query_path = "/S4"
     channel_query_path = "/CH4"
 
-    UA = "Line/8.0.0"
-    LA = "CHROMEOS\t8.0.0\tChrome_OS\t1"
+    UA = "Line/7.18.0"
+    LA = "CHROMEOS\t7.18.0\tChrome_OS\t1"
 
     authToken = None
     mid = None
