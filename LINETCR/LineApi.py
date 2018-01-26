1ff55182811c7f924bbf9e1923d31e87fc651a2f
Selfbotman <34406024+Selfbotman@users.noreply.github.com>
2018-01-26 16:24:46 +0700

    Update LineApi.py

--- LINETCR/LineApi.py
+++ LINETCR/LineApi.py
@@ -41,11 +41,12 @@ class LINE:
       raise Exception("invalid arguments")
 
     self.authToken = self.Talk.authToken
+    self.headers = {'X-Line-Application': 'CHROMEOS\t8.0.0\tChrome_OS\t1', 'X-Line-Access': self.authToken, 'User-Agent': 'Line/8.0.0 iPad4,1 9.0.2'}
     self.cert = self.Talk.cert
     self._headers = {
-              'X-Line-Application': 'IOSIPAD\t7.14.0\tiPhone OS\t10.12.0',
+              'X-Line-Application': 'CHROMEOS\t8.0.0\tChrome_OS\t1',
               'X-Line-Access': self.authToken,
-              'User-Agent': 'Line/7.14.0'
+              'User-Agent': 'Line/8.0.0 iPad4,1 9.0.2'
    }
 
     self.Poll = Poll(self.authToken)
