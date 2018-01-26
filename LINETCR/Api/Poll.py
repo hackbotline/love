3d1f8fb89c561743f107cb097a6487ac7d427b93
Selfbotman <34406024+Selfbotman@users.noreply.github.com>
2018-01-27 01:32:55 +0700

    Update Poll.py

--- LINETCR/Api/Poll.py
+++ LINETCR/Api/Poll.py
@@ -18,8 +18,8 @@ class Poll:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = "Line/8.0.0"
-  LA = "CHROMEOS\t8.0.0\tChrome_OS\t1"
+  UA = "Line/7.18.0"
+  LA = "CHROMEOS\t7.18.0\tChrome_OS\t1"
 
   rev = 0
 
